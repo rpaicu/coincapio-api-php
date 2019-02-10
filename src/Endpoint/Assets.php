5e4bf4c1e4e224e163833085d1b6f4ab753066f6
Paul Rock <paul@drteam.rocks>
2019-02-10 22:50:03 +0300

    method markets added to Assets class

--- src/Endpoint/Assets.php
+++ src/Endpoint/Assets.php
@@ -19,18 +19,42 @@ use GuzzleHttp\Psr7\Request;
 class Assets extends Client
 {
     /**
-     * Get all assets
+     * Get a list of all assets
      *
-     * @param   array $options
+     * @example curl --location --request GET "api.coincap.io/v2/assets" --data ""
+     *
+     * @param   string|null $search search by asset id (bitcoin) or symbol (BTC)
+     * @param   string|null $ids    query with multiple ids=bitcoin,ethereum,monero
+     * @param   int|null    $limit  max limit of 2000
+     * @param   int|null    $offset offset
      * @return  array
      * @throws  \GuzzleHttp\Exception\GuzzleException
+     * @since   0.1
      */
-    public function all(array $options = []): array
+    public function all(string $search = null, string $ids = null, int $limit = null, int $offset = null): array
     {
-        // List of allowed options
-        $allowed_options = ['search', 'limit', 'offset'];
-        // Check if options is in allowed list of current method
-        $this->checkOptions(array_keys($options), $allowed_options);
+        // List of all options
+        $options = [];
+
+        // If search is set
+        if (null !== $search) {
+            $options['search'] = $search;
+        }
+
+        // If ids is set
+        if (null !== $ids) {
+            $options['ids'] = $ids;
+        }
+
+        // If start is set
+        if (null !== $limit) {
+            $options['limit'] = $limit;
+        }
+
+        // If end is set
+        if (null !== $offset) {
+            $options['offset'] = $offset;
+        }
 
         $req = new Request('GET', 'assets');
         return $this->doRequest($req, ['query' => $options]);
@@ -39,13 +63,14 @@ class Assets extends Client
     /**
      * Get asset by id
      *
-     * @param   string $id
+     * @param   string $id asset id
      * @return  array
      * @throws  \GuzzleHttp\Exception\GuzzleException
+     * @since   0.1
      */
     public function get(string $id): array
     {
-        $req = new Request('GET', \sprintf('assets/%s', $id));
+        $req  = new Request('GET', \sprintf('assets/%s', $id));
         $data = $this->doRequest($req);
         return \array_shift($data);
     }
@@ -53,20 +78,69 @@ class Assets extends Client
     /**
      * Get history of ticker by id
      *
-     * @param   string $id
-     * @param   array $options
+     * @example curl --location --request GET "api.coincap.io/v2/assets/bitcoin/history?interval=d1" --data ""
+     *
+     * @param   string   $id       asset id
+     * @param   string   $interval point-in-time interval. minute and hour intervals represent
+     *                             price at that time, the day interval represents average of 24
+     *                             hour periods (timezone: UTC)
+     *                             Example: m1, m5, m15, m30, h1, h2, h6, h12, d1
+     * @param   int|null $start    (optional) UNIX time in milliseconds. omiting will return the most
+     *                             recent asset history. If start is supplied, end is required and vice vera
+     * @param   int|null $end      (optional) UNIX time in milliseconds. omiting will return the most
+     *                             recent asset history. If start is supplied, end is required and vice vera
      * @return  array
      * @throws  Exception
      * @throws  \GuzzleHttp\Exception\GuzzleException
+     * @since   0.1
      */
-    public function history(string $id, array $options = []): array
+    public function history(string $id, string $interval = '1d', int $start = null, int $end = null): array
     {
-        // List of allowed options
-        $allowed_options = ['interval', 'id', 'start', 'end', 'limit', 'offset'];
-        // Check if options is in allowed list of current method
-        $this->checkOptions(array_keys($options), $allowed_options);
+        // List of all options
+        $options['interval'] = $interval;
+
+        // If start is set
+        if (null !== $start) {
+            $options['start'] = $start;
+        }
+
+        // If end is set
+        if (null !== $end) {
+            $options['end'] = $end;
+        }
 
         $req = new Request('GET', \sprintf('assets/%s/history', $id));
+        return $this->doRequest($req, ['query' => $options]);
+    }
+
+    /**
+     * Get historical data of some ticker
+     *
+     * @example curl --location --request GET "api.coincap.io/v2/assets/bitcoin/markets" --data ""
+     *
+     * @param   string   $id     asset id
+     * @param   int|null $limit  max limit of 2000
+     * @param   int|null $offset offset
+     * @return  array
+     * @throws  \GuzzleHttp\Exception\GuzzleException
+     * @since   0.2
+     */
+    public function markets(string $id, int $limit = null, $offset = null): array
+    {
+        // List of all options
+        $options = [];
+
+        // If start is set
+        if (null !== $limit) {
+            $options['limit'] = $limit;
+        }
+
+        // If end is set
+        if (null !== $offset) {
+            $options['offset'] = $offset;
+        }
+
+        $req = new Request('GET', \sprintf('assets/%s/markets', $id));
         return $this->doRequest($req, ['query' => $options]);
     }
 }
